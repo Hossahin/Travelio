4687e12482835135a97aab7d402b4956a8345375
HossahinDev <hossahin321@gmail.com>
2025-09-27 15:31:16 +0600

    just hello

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-
-
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      Hello, Next.js!
     </div>
   );
 }
