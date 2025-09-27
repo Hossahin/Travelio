7cd910d434b2c9441120e31b8e5c4f4c9760c6b3
HossahinDev <hossahin321@gmail.com>
2025-09-27 15:24:45 +0600

    test 1223

--- src/app/page.js
+++ src/app/page.js
@@ -3,30 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl font-bold text-center">
-        We Are make something new project
-      </h1>
-      <p className="text-center text-gray-500 mt-4">
-        Are you ready to make something new?
-      </p>
-
-      <div className="mt-8 w-full mx-auto bg-gray-500 rounded-2xl text-center p-8 border border-gray-700">
-        <h1 className="text-xl font-bold text-white">Welcome to Travelio</h1>
-        <p className="text-white">
-          Your one-stop solution for all your travel needs.
-        </p>
-        <p>hello are you okay</p>
-        <p>i am fine</p>
-        <p>oh nice and you</p>
-
-        <h1>Hello test</h1>
-      </div>
-
-      <div className="mt-8 w-full mx-auto bg-gray-500 rounded-2xl text-center p-8 border border-gray-700">
-        <h1 className="text-xl font-bold text-white">Hello test</h1>
-
-        <h3 className="text-lg font-bold text-white">Test 123</h3>
-      </div>
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+      <p className="mt-4">Welcome to your new Next.js app!</p>
 
     </div>
   );
